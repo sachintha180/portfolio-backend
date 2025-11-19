a8327018ef7e2c13ee62a5c2cfab1e173720c8aa
sachintha180 <sachinthasenanayake180@gmail.com>
2025-11-20 00:21:12 +0530

    add authentication routes, add common route-level dependencies

--- routes/user.py
+++ routes/user.py
@@ -1,41 +1,68 @@
 from uuid import UUID
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 
-from schemas.user import UserUpdate
-from models.user import User
-from custom_types.dependencies import AuthenticatedContextDep, UserServiceDep
+from schemas.user import (
+    UserUpdateRequest,
+    UserGetResponse,
+    UserUpdateResponse,
+)
+from custom_types.dependencies import DBSessionDep, UserServiceDep
+from api.dependencies import (
+    get_authenticated_user,
+    get_user_service,
+)
 
-router = APIRouter(prefix="/users", tags=["users"])
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    dependencies=[
+        Depends(get_authenticated_user),
+        Depends(get_user_service),
+    ],
+)
 
 
-@router.get("/{user_id}", response_model=User)
+@router.get(
+    "/{user_id}",
+    response_model=UserGetResponse,
+    status_code=status.HTTP_200_OK,
+)
 def get_user(
     user_id: UUID,
-    service: UserServiceDep,
-    ctx: AuthenticatedContextDep,
+    user_service: UserServiceDep,
+    db_session: DBSessionDep,
 ):
     """Get a user by ID."""
-    return service.get_user_by_id(ctx.session, user_id)
+    user = user_service.get_user_by_id(db_session, user_id)
+
+    return UserGetResponse(user=user)
 
 
-@router.patch("/{user_id}", response_model=User)
+@router.patch(
+    "/{user_id}",
+    response_model=UserUpdateResponse,
+    status_code=status.HTTP_200_OK,
+)
 def update_user(
     user_id: UUID,
-    user_data: UserUpdate,
-    service: UserServiceDep,
-    ctx: AuthenticatedContextDep,
+    request_data: UserUpdateRequest,
+    user_service: UserServiceDep,
+    db_session: DBSessionDep,
 ):
     """Update a user."""
-    return service.update_user(ctx.session, user_id, user_data)
+    user = user_service.update_user(db_session, user_id, request_data)
+
+    return UserUpdateResponse(user=user)
 
 
 @router.delete(
-    "/{user_id}", status_code=status.HTTP_204_NO_CONTENT
-)  # NOTE: Returns 204 because there's no response body
+    "/{user_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
 def delete_user(
     user_id: UUID,
-    service: UserServiceDep,
-    ctx: AuthenticatedContextDep,
+    user_service: UserServiceDep,
+    db_session: DBSessionDep,
 ):
     """Delete a user."""
-    service.delete_user(ctx.session, user_id)
+    user_service.delete_user(db_session, user_id)
